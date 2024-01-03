fa9c91828c539901da717f665fab87ad93e78a26
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-03 12:42:42 -0500

    task 0 v1

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+from __future__ import annotations
+
 
 def add(a: float, b: float) -> float:
     return a + b
 
-add = __import__('0-add').add
+# add = __import__('0-add').add
+
 
 print(add(1.11, 2.22) == 1.11 + 2.22)
 print(add.__annotations__)
