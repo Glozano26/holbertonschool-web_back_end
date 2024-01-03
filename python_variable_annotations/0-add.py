8f9f9278646395bdcdeafa1fe126fe98ab63ce65
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-03 12:49:01 -0500

    task 0 v3

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -6,9 +6,3 @@ and a float b as arguments and returns their sum as a float."""
 def add(a: float, b: float) -> float:
     """Return the sum of a 2 floats"""
     return a + b
-
-# add = __import__('0-add').add
-
-
-print(add(1.11, 2.22) == 1.11 + 2.22)
-print(add.__annotations__)
