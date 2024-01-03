0a11d32763883c07a634b1a3231cafdfe1c687ad
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-03 13:04:05 -0500

    task 1 v2

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -5,11 +5,5 @@ and a string str2 as arguments and returns a concatenated string"""
 
 def concat(str1: str, str2: str) -> str:
     """returns a concatenated string"""
+
     return str1 + str2
-
-
-str1 = "egg"
-str2 = "shell"
-
-print(concat(str1, str2) == "{}{}".format(str1, str2))
-print(concat.__annotations__)
