63f1848fafdef7c9380a4ff0d46592c34752c4a0
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-03 12:47:56 -0500

    task 0 v2

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-from __future__ import annotations
+"""Write a type-annotated function add that takes a float a
+and a float b as arguments and returns their sum as a float."""
 
 
 def add(a: float, b: float) -> float:
+    """Return the sum of a 2 floats"""
     return a + b
 
 # add = __import__('0-add').add
