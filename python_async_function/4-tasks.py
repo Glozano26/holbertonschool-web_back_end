ed85407e904ca09054d220dd77f7550f3d341600
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-05 13:40:37 -0500

    task 4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+import asyncio
+import random
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Execute wait_random coroutine 'n' times"""
+    delays = []
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    return delays
