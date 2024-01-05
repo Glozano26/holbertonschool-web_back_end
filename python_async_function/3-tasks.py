a771e70e7441ff33dac7f5d877ea3cc656856318
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-05 13:21:50 -0500

    Tasks

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Write a function (do not create an async function, use the regular
+function syntax to do this) task_wait_random that takes an integer
+max_delay and returns a asyncio.Task"""
+import asyncio
+from asyncio import Task
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Task:
+    """Get the result of the wait_random coroutine"""
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(wait_random(max_delay))
+    # loop.run_until_complete(task)
+    return task
