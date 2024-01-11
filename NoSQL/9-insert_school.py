eb62fddfd277e18369b600cf82fb56c184822ee1
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-11 11:49:41 -0500

    task 9

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -9,4 +9,4 @@ a collection based on kwargs
 def insert_school(mongo_collection, **kwargs):
     """Insert a document in Python"""
     result = mongo_collection.insert_one(kwargs)
-    return result._id
+    return result.inserted_id
---
+++ python_async_function/example1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""This example is Synchronous and subrutine"""
+import time
+
+def brewCoffee():
+    print("Start brewCoffee()")
+    time.sleep(3)
+    print("End brewCoffee()")
+    return "Coffee ready"
+
+def toastBagel():
+    print("Start toastBagel()")
+    time.sleep(2)
+    print("End toastBagel()")
+    return "Bagel toasted"
+
+def main():
+    start_time = time.time()
+
+    result_coffee = brewCoffee()
+    result_bagel = toastBagel()
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    print(f"Result of brewCoffee: {result_coffee}")
+    print(f"Result of toastBagel: {result_bagel}")
+    print(f"Total execution time: {elapsed_time}")
+
+if __name__ == "__main__":
+    main()
