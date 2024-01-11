d73f1a080942dd14cbe6ea8194e15b5be316da1c
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-10 20:21:24 -0500

    List all documents in Python v1

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -4,5 +4,4 @@
 
 def list_all(mongo_collection):
     """Return an empty list if no document in the collection"""
-    for x in mongo_collection.find([]):
-        return (x)
+    return [x for x in mongo_collection.find()]
