bb8e5e92ca7f0a406eb59b4c7ea49c7f51f56be3
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-10 21:30:20 -0500

    Insert a document in Python v1

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -8,4 +8,5 @@ a collection based on kwargs
 
 def insert_school(mongo_collection, **kwargs):
     """Insert a document in Python"""
-    return [x for x in kwargs.mongo_collection()]
+    result = mongo_collection.insert_one(kwargs)
+    return result._id
