39d29ce7899119bceb02feccbd85561abdc39db9
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-11 17:13:09 -0500

    task 11 v6

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -8,10 +8,4 @@ school having a specific topic:
 
 def schools_by_topic(mongo_collection, topic):
     """returns a list with a specific topic"""
-    topic_search = mongo_collection.find({'topic': topic})
-
-    schools = []
-    for school in topic_search:
-        schools.append(school)
-
-    return schools
+    return list([x for x in mongo_collection.find({'topic': topic})])
