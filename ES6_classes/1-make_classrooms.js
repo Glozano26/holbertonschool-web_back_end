554ed076992977959b4336ab03456ca42f8fa41f
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-21 22:36:12 -0500

    task 1 v0

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,7 +1,9 @@
 import ClassRoom from "./0-classroom";
 
 export default function initializeRooms(){
-  const class1 = new ClassRoom();
-  const class2 = new ClassRoom();
-  const class3 = new ClassRoom();
+  const class1 = new ClassRoom(19);
+  const class2 = new ClassRoom(20);
+  const class3 = new ClassRoom(34);
+
+  return [class1, class2, class3]
 }
