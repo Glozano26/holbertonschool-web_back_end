4f6c57298445ca36e461c235ca539a8d0cd0da64
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-23 10:08:22 -0500

    A Building v2

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,13 +1,14 @@
 export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building) {
+      throw new TypeError('Class extending Building must override evacuationWarningMessage');
+    }
+    if (!this.evacuationWarningMessage) {
+      throw new TypeError('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
-
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new TypeError('Class extending Building must override evacuationWarningMessage');
   }
 }
