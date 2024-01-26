47f7b65b672aee8baf593b161e1d86f71b86bf27
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-26 18:57:36 -0500

    Reject the promises v1

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,10 +1,6 @@
 export default function uploadPhoto(filename) {
   return new Promise((resolve, reject) => {
-    const filename = false;
-    if (filename) {
-      resolve('succes!');
-    } else {
-        reject(new Error(`${filename} cannot be processed`));
-    }
+    // const filename = false;
+    reject(new Error(`${filename} cannot be processed`));
   })
 }
