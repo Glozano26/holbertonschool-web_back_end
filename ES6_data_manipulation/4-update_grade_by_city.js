3cd9713f430ceead7bdd6e694871182f4f2ba13d
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-31 18:17:40 -0500

    Combine

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,8 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const filtered = students.filter((student) => student.location === city);
+  
+  return filtered.map((student) => ({
+    ...student,
+    grade: newGrades.find((grade) => grade.studentId === student.id)?.grade ?? 'N/A',
+  }));
+}
