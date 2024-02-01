f61022f3047af702cd93ccd8fbfdb37b7b796f92
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-31 19:04:31 -0500

    Combine v3

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,26 @@
-/* eslint-disable */
-export default function updateStudentGradeByCity(students, city, newGrades) {
-  const filtered = students.filter((student) => student.location === city);
+// export default function updateStudentGradeByCity(students, city, newGrades) {
+//   const filtered = students.filter((student) => student.location === city);
 
-  return filtered.map((student) => ({
-    ...student,
-    grade: newGrades.find((grade) => grade.studentId === student.id)?.grade ?? 'N/A',
-  }));
+//   return filtered.map((student) => ({
+//     ...student,
+//     grade: newGrades.find((grade) => grade.studentId === student.id)?.grade ?? 'N/A',
+//   }));
+// }
+export default function updateStudentGradeByCity(listStudents, city, newGrades) {
+  if (Array.isArray(listStudents)) {
+    const newArray = listStudents.filter((value) => value.location === city);
+    const arraUpdate = newArray.map((value) => {
+      const obj = newGrades.find((val) => val.studentId === value.id);
+      if (obj) {
+        /* eslint-disable no-param-reassign */
+        value.grade = obj.grade;
+      } else {
+        /* eslint-disable no-param-reassign */
+        value.grade = 'N/A';
+      }
+      return value;
+    });
+    return arraUpdate;
+  }
+  return NaN;
 }
-/* eslint-disable */
