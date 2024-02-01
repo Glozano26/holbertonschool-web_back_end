6b66d341a2e2d595ad836cf8d48df841b36b426d
Glozano26 <lozsuperbia2014@gmail.com>
2024-01-31 19:50:37 -0500

    task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,8 +1,27 @@
 import signUpUser from './4-user-promise';
-import { uploadPhoto } from './utils';
+import uploadPhoto from './5-photo-reject';
 
-export default asyn function handleProfileSignup(firstName, lastName, fileName) {
-  const promise = new Promise.all([signUpUser(), uploadPhoto()]) => {
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const promise1 = {};
+  const promise2 = {};
 
+  try {
+    promise1.value = await signUpUser(firstName, lastName);
+    promise1.status = 'fulfilled';
+  } catch (err) {
+    promise1.value = err.toString();
+    promise1.status = 'rejected';
   }
+
+  try {
+    promise2.value = await uploadPhoto(fileName);
+    promise2.status = 'fulfilled';
+  } catch (err) {
+    promise2.value = err.toString();
+    promise2.status = 'rejected';
+  }
+
+  return [promise1, promise2];
 }
+
+export default handleProfileSignup;
